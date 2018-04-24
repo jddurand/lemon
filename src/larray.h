78ecd25c33f7ea8b3ad37462ceeac0944e32c98e
Jean-Damien Durand <jeandamiendurand@free.fr>
2018-04-24 06:27:52 +0200

    export dev+

--- src/larray.h
+++ src/larray.h
@@ -1,7 +1,8 @@
 #ifndef LEMON_LARRAY_H
 #define LEMON_LARRAY_H
 
-#include "lobject.h"
+#include <lemon_export.h>
+#include <lobject.h>
 
 struct larray {
 	struct lobject object;
@@ -11,34 +12,49 @@ struct larray {
 	struct lobject **items;
 };
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+LEMON_EXPORT
 struct lobject *
 larray_append(struct lemon *lemon,
               struct lobject *self,
               int argc, struct lobject *argv[]);
 
+LEMON_EXPORT
 struct lobject *
 larray_get_item(struct lemon *lemon,
                 struct lobject *self,
                 long i);
 
+LEMON_EXPORT
 struct lobject *
 larray_set_item(struct lemon *lemon,
                 struct lobject *self,
                 long i,
                 struct lobject *value);
 
+LEMON_EXPORT
 struct lobject *
 larray_del_item(struct lemon *lemon,
                 struct lobject *self,
                 long i);
 
+LEMON_EXPORT
 long
 larray_length(struct lemon *lemon, struct lobject *self);
 
+LEMON_EXPORT
 void *
 larray_create(struct lemon *lemon, int count, struct lobject *items[]);
 
+LEMON_EXPORT
 struct ltype *
 larray_type_create(struct lemon *lemon);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* LEMON_LARRAY_H */
--- src/lstring.h
+++ src/lstring.h
@@ -1,7 +1,8 @@
 #ifndef LEMON_LSTRING_H
 #define LEMON_LSTRING_H
 
-#include "lobject.h"
+#include <lemon_export.h>
+#include <lobject.h>
 
 struct lstring {
 	struct lobject object;
@@ -12,19 +13,32 @@ struct lstring {
 	char buffer[1];
 };
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+LEMON_EXPORT
 const char *
 lstring_to_cstr(struct lemon *lemon, struct lobject *object);
 
+LEMON_EXPORT
 char *
 lstring_buffer(struct lemon *lemon, struct lobject *object);
 
+LEMON_EXPORT
 long
 lstring_length(struct lemon *lemon, struct lobject *object);
 
+LEMON_EXPORT
 void *
 lstring_create(struct lemon *lemon, const char *buffer, long length);
 
+LEMON_EXPORT
 struct ltype *
 lstring_type_create(struct lemon *);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* LEMON_LSTRING_H */
